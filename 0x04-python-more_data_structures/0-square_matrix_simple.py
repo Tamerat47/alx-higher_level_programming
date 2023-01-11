01796423f0c2a2eb5c816e39a14e7a37490cdb91
Tamerat47 <tameratmekonnen70@gmail.com>
2023-01-11 23:20:37 +0300

    holb suck

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    while len(matrix) > 0:
+        for elems in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, elems)))
+
+    return new_matrix
