e0d70be93892f5f0b0ec6936dbb045a5487c9863
Tamerat47 <98102069+Tamerat47@users.noreply.github.com>
2023-03-01 01:13:42 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""Defines an empty class BaseGeometry."""
+
+
 class BaseGeometry:
-    def area(self):
-        raise Exception('area() is not implemented')
+    """Represent base geometry."""
+    pass
